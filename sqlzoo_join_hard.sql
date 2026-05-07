86ab167649783ba68d6524e13dd4b940dca9448c
Michael Allen <mike.allen@sky.com>
2026-05-07 16:54:56 +0100

    feat: restore sqlzoo join challenge to correct repo

---
+++ sqlzoo_join_hard.sql
@@ -0,0 +1,8 @@
+SELECT mdate, team1,
+ SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) AS score1,
+ team2,
+ SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) AS score2
+ FROM game LEFT JOIN goal ON (id=matchid)
+ WHERE team1 ='ENG' OR team2 = 'ENG'
+ GROUP BY mdate, matchid, team1, team2
+ ORDER BY mdate, matchid, team1, team2
